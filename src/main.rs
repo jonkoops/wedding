daa37cf7d1cc5785c2578228fa1578f507724969
Jon Koops <jonkoops@gmail.com>
2024-04-01 12:29:21 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use askama::Template;
 use salvo::prelude::*;
+use salvo::session::{CookieStore, SessionHandler};
+
+static DOMAIN: &str = "localhost";
+static COOKIE_SECRET: &[u8] = b"secretabsecretabsecretabsecretabsecretabsecretabsecretabsecretab";
 
 #[derive(Template)]
 #[template(path = "index.html")]
@@ -25,9 +29,18 @@ async fn rsvp(res: &mut Response) {
 async fn main() {
     tracing_subscriber::fmt().init();
 
+    let session_handler = SessionHandler::builder(CookieStore::new(), COOKIE_SECRET)
+        .cookie_name("session")
+        .cookie_domain(DOMAIN)
+        .same_site_policy(salvo::http::cookie::SameSite::Strict)
+        .build()
+        .unwrap();
+
     let router = Router::new()
+        .hoop(session_handler)
         .get(index)
         .push(Router::with_path("rsvp").get(rsvp));
+
     let acceptor = TcpListener::new(("0.0.0.0", 3000)).bind().await;
 
     Server::new(acceptor).serve(router).await;
